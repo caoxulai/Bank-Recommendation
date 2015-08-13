42eb93d2f37c4fda206c70bd72a40e6e1be507c6
Nathan Cao <nathan@novramedia.com>
2015-08-13 13:14:50 -0400

    Matlab   cluster and analyze training dataset, build up final training set.

---
+++ server/matlab/l_to_i.m
@@ -0,0 +1,29 @@
+function index = l_to_i(label)
+switch(label)
+    case 11
+        index = 1;
+    case 12
+        index = 2;
+    case 13
+        index = 3;
+    case 21
+        index = 5;
+    case 22
+        index = 6;
+    case 23
+        index = 7;
+    case 31
+        index = 9;
+    case 32
+        index = 10;
+    case 41
+        index = 12;
+    case 42
+        index = 13;
+    case 51
+        index = 15; 
+    case 52
+        index = 16; 
+    case 53
+        index = 17;
+end
